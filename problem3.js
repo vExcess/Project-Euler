3da24bd1ec4fc6b442516ae8f4817156216ba79a
vExcess <41875927+vExcess@users.noreply.github.com>
2023-06-29 17:47:07 -0700

    Update problem3.js

--- problem3.js
+++ problem3.js
@@ -1,46 +1,21 @@
-var primeFactors = [];
-var number = 0;
-
-var prime = function(number) {
-    for(var i = 2; i < sqrt(number); i++) {
-        if(number % i === 0){
-            return false;
-        }
-    }
-    return true;
-};
-
-frameRate(max);
-textAlign(CENTER);
-textSize(20);
-
-frameRate(250);
-var draw = function(){
-	background(255, 255, 255);
-    
-    
-    fill(0, 0, 0);
-    text(number, 300, 100);
-    
-    text(((number/775146)*100) + "%", 300, 150);
-    
-    fill(0, 0, 0);
-    rect(50, 200, 500, 20);
-    
-    fill(4, 255, 0);
-    rect(50, 200, (number/1551), 20);
-    
-    if(number <= 775146){
-        for(var j = 0; j < 5000; j++){
-            if(prime(number+j) && 600851475143 % (number+j) === 0) {
-                primeFactors.push(number+j);
+// jshint ignore: start
+let start = performance.now();
+let primeFactors = [];
+let sqrt = Math.sqrt;
+for (let num = 4, stop = sqrt(600851475143); num < stop; num++) {
+    if (600851475143 % num === 0) {
+        let i = 5;
+        let is_prime = true;
+        let sqNum = sqrt(num);
+        while (i <= sqNum) {
+            if (num % i === 0 || num % (i + 2) === 0) {
+                is_prime = false;
+                break;
             }
+            i += 6;
         }
-        
-        number += 5000;
-    }else{
-        println(primeFactors);
-        noLoop();
+        if (is_prime) primeFactors.push(num);
     }
-
-};
+}
+let end = performance.now();
+console.log(primeFactors.slice(-1)[0], (end - start) + "ms")
